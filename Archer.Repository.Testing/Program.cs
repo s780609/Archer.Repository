6604e5d4d28a58ddd687dc7447df82a0b15bf367
hsutinghuan <s780609@gmail.com>
2024-05-17 11:42:04 +0800

    fix: fix bug in Update method
    what?
        -- Fix bug in Update method that model and key are not merged
        -- Modify Create, Update, and Delete method

--- Archer.Repository.Testing/Program.cs
+++ Archer.Repository.Testing/Program.cs
@@ -5,15 +5,15 @@ using Uitc.Repository.Testing;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Archer.Repository.Testing Start...");
 
-const string keyConn = "keyConn";
-const string ivConn = "ivConn";
-const string keyData = "keyData";
-const string ivData = "ivData";
+const string keyConn = "     ";
+const string ivConn = "      ";
+const string keyData = "     ";
+const string ivData = "      ";
 SecurityHelper securityHelper = new SecurityHelper(keyConn, ivConn, keyData, ivData);
 
 Repository repository = new Repository("", securityHelper);
 
-var loans = repository.Query<Loan>();
+var loan = repository.Query<Loan>(param: new { LoanID = "20240517A2205193254001" }).Single();
 
 var groupCodes = repository.Query<GroupCodes>("SELECT * FROM GroupCode;");
 
