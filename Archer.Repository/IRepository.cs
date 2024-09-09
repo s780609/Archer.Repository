9acf8f9cc11ebc0247ebb68e46903ab7859dd21a
TinghuanHsu <tinghuanhsu@uitctech.com.tw>
2024-09-09 17:18:22 +0800

    feat: add isolation level parameter in all methods

--- Archer.Repository/IRepository.cs
+++ Archer.Repository/IRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data;
 
 namespace Archer.Repository
 {
@@ -9,7 +10,7 @@ namespace Archer.Repository
         /// </summary>
         /// <param name="sql"></param>
         /// <returns></returns>
-        int Execute(string sql, object param = null);
+        int Execute(string sql, object param = null, IsolationLevel isolationLevel = IsolationLevel.Serializable);
 
         /// <summary>
         /// Base query. T is generic
@@ -18,9 +19,9 @@ namespace Archer.Repository
         /// <param name="sql"></param>
         /// <param name="param"></param>
         /// <returns></returns>
-        List<Table> Query<Table>(string sql = "", object param = null);
+        List<Table> Query<Table>(string sql = "", object param = null, IsolationLevel isolationLevel = IsolationLevel.Serializable);
 
-        Table QuerySingle<Table>(string sql = "", object param = null);
+        Table QuerySingle<Table>(string sql = "", object param = null, IsolationLevel isolationLevel = IsolationLevel.Serializable);
 
         /// <summary>
         /// 幫助產生 where 敘述，會自動產生 [參數=@參數] 的敘述
@@ -41,18 +42,18 @@ namespace Archer.Repository
 
         string FindTableName<Table>(string regexPattern = "^(.+)$");
 
-        int Create<Table>(Table model);
+        int Create<Table>(Table model, IsolationLevel isolationLevel = IsolationLevel.Serializable);
 
-        int Create<Table>(object model);
+        int Create<Table>(object model, IsolationLevel isolationLevel = IsolationLevel.Serializable);
 
         void Create<Table>(IEnumerable<Table> modelList);
 
-        int Update<Table>(Table model, Table key);
+        int Update<Table>(Table model, Table key, List<RepositoryOption> options = null, IsolationLevel isolationLevel = IsolationLevel.Serializable);
 
-        int Update<Table>(object model, object key, List<RepositoryOption> options = null);
+        int Update<Table>(object model, object key, List<RepositoryOption> options = null, IsolationLevel isolationLevel = IsolationLevel.Serializable);
 
-        int Delete<Table>(Table model);
+        int Delete<Table>(Table model, IsolationLevel isolationLevel = IsolationLevel.Serializable);
 
-        int Delete<Table>(object model);
+        int Delete<Table>(object model, IsolationLevel isolationLevel = IsolationLevel.Serializable);
     }
 }
